fba87587e58d6a54a554517149f799d4bb20852b
lukaszewskik <51677513+lukaszewskik@users.noreply.github.com>
2021-03-01 20:43:09 +0200

    Node and grid classes

---
+++ Grid.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+
+    public Node[,] nodes;
+    public int width = 8; //columns, X
+    public int height = 10; //rows, Y
+
+    
+    void Start()
+    {
+        nodes = new Node[width, height];
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                nodes[j, i] = new Node();
+                nodes[j, i].SetNodePosition(j, i);
+
+                if (j>0)
+                {                    
+                    nodes[j, i].SetLeftNode(nodes[j - 1, i]);                    
+                }
+
+                if (i>0)
+                {
+                    nodes[j, i].SetBottomNode(nodes[j, i - 1]);
+
+                }               
+            }
+        }
+    }
+
+  
+    private void OnDrawGizmos()
+    {
+        if (nodes.GetLength(0) > 0)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Gizmos.DrawSphere(nodes[j, i].position, 0.2f);
+                }
+            }
+        }
+        
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node  
+{
+    public int gCost; 
+    public int hCost; 
+    public int fCost; 
+
+    public Node leftNode;
+    public Node bottomNode;
+
+    public Vector3 position;
+
+    public void SetNodePosition(int x, int y)
+    {
+        position = new Vector3(x, y, 0);
+    }
+
+    public void SetLeftNode(Node node)
+    {
+        leftNode = node;
+    }
+
+    public void SetBottomNode(Node node)
+    {
+        bottomNode = node;
+    }
+
+    
+
+}
